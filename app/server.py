9beaf06a1f896ff60593df2e78a0740ef4d1f116
Deegan <Deegan.Tom@bcg.com>
2019-10-01 17:13:44 -0400

    Fix Syntax Error

--- app/server.py
+++ app/server.py
@@ -62,7 +62,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     #img = open_image(BytesIO(img_bytes))
     #prediction = learn.predict(img)[0]
-    prediction = "\n".join(learn.predict(content, 40, temperature=0.75) for _ in range(1))
+    prediction = "\n".join(learn.predict("Matt Damon stars", 40, temperature=0.75) for _ in range(1))
     return JSONResponse({'result': str(prediction)})
 
 
